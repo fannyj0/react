5fc8dd1376bffc5c2e2d0ad4a0a6cde166218b3d
fannyj0 <fanny.jokinen@metropolia.fi>
2020-03-18 19:12:37 +0200

    tehty

---
+++ src/components/catRow.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CatRow = ({file}) => {
+  return (
+    <tr>
+      <td>
+        <img src={file.thumbnails.w160} alt={file.title}/>
+      </td>
+      <td>
+        <h3>{file.title}</h3>
+        <p>{file.description}</p>
+      </td>
+      <td>
+        <a href={file.filename}>View</a>
+      </td>
+    </tr>);
+};
+
+
+CatRow.propTypes = {
+  file: PropTypes.object,
+};
+
+export default CatRow;
