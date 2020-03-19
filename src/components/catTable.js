1be2fcd0dad0291c02938763a22f864753b6c713
fannyj0 <fanny.jokinen@metropolia.fi>
2020-03-19 10:51:34 +0200

    teht2 valmis

--- src/components/catTable.js
+++ src/components/catTable.js
@@ -1,18 +1,27 @@
 import React, {useState, useEffect} from 'react';
-import PropTypes from 'prop-types';
 import CatRow from './catRow';
+
+const baseUrl = 'http://media.mw.metropolia.fi/wbma/';
 
 const CatTable = () => {
   const [picArray, setPicArray] = useState([]);
   const loadMedia = async () => {
-    const response = await fetch('test.json');
+    const response = await fetch(baseUrl + 'media');
     const json = await response.json();
-    console.log(json);
-    setPicArray(json);
+
+    // haetaan yksittäiset kuvat, jotta saadaan thumbnailit
+    const items = await Promise.all(json.map(async (item) => {
+      const response = await fetch(baseUrl + 'media/' + item.file_id);
+      return await response.json();
+    }));
+    console.log(items);
+    setPicArray(items);
   };
+
   useEffect(()=>{
     loadMedia();
   }, []);
+
   return (
     <table>
       <tbody>
@@ -24,8 +33,5 @@ const CatTable = () => {
   );
 };
 
-CatTable.propTypes = {
-  media: PropTypes.array,
-};
 
 export default CatTable;
